d6d7ed60f1ceab56b40945f3f240a448fedb4db1
renxinxie <1700017774@pku.edu.cn>
2021-12-03 22:01:20 +0800

    initialize

---
+++ README.md
@@ -0,0 +1,2 @@
+# cs61A-Fall2021
+Personal Study Record
---
+++ lab00/lab00.py
@@ -0,0 +1,8 @@
+def twenty_twenty_one():
+    """Come up with the most creative expression that evaluates to 2021,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty_one()
+    2021
+    """
+    return ______
