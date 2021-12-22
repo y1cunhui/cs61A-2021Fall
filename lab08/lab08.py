0c7888c44382958d368b4af93c84b09c3ce87001
y1cunhui <1700017774@pku.edu.cn>
2021-12-22 12:20:08 +0800

    lab08 finished

---
+++ lab08/lab08.py
@@ -0,0 +1,252 @@
+def convert_link(link):
+    """Takes a linked list and returns a Python list with the same elements.
+
+    >>> link = Link(1, Link(2, Link(3, Link(4))))
+    >>> convert_link(link)
+    [1, 2, 3, 4]
+    >>> convert_link(Link.empty)
+    []
+    """
+    "*** YOUR CODE HERE ***"
+    # recursive
+    if link is Link.empty:
+      return []
+    else:
+      return [link.first] + convert_link(link.rest)
+    
+    # iterative
+    # ans = []
+    # now_link = link
+    # while now_link != Link.empty:
+    #   ans.append(now_link.first)
+    #   now_link = now_link.rest
+    # return ans 
+
+
+def label_squarer(t):
+    """Mutates a Tree t by squaring all its elements.
+
+    >>> t = Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
+    >>> label_squarer(t)
+    >>> t
+    Tree(1, [Tree(9, [Tree(25)]), Tree(49)])
+    """
+    "*** YOUR CODE HERE ***"
+    t.label = t.label**2
+    for branch in t.branches:
+      label_squarer(branch)
+
+
+def cumulative_mul(t):
+    """Mutates t so that each node's label becomes the product of all labels in
+    the corresponding subtree rooted at t.
+
+    >>> t = Tree(1, [Tree(3, [Tree(5)]), Tree(7)])
+    >>> cumulative_mul(t)
+    >>> t
+    Tree(105, [Tree(15, [Tree(5)]), Tree(7)])
+    """
+    "*** YOUR CODE HERE ***"
+    mul = 1
+    for branch in t.branches:
+      cumulative_mul(branch)
+      mul *= branch.label
+    t.label *= mul
+    
+
+
+
+def add_d_leaves(t, v):
+    """Add d leaves containing v to each node at every depth d.
+
+    >>> t_one_to_four = Tree(1, [Tree(2), Tree(3, [Tree(4)])])
+    >>> print(t_one_to_four)
+    1
+      2
+      3
+        4
+    >>> add_d_leaves(t_one_to_four, 5)
+    >>> print(t_one_to_four)
+    1
+      2
+        5
+      3
+        4
+          5
+          5
+        5
+
+    >>> t1 = Tree(1, [Tree(3)])
+    >>> add_d_leaves(t1, 4)
+    >>> t1
+    Tree(1, [Tree(3, [Tree(4)])])
+    >>> t2 = Tree(2, [Tree(5), Tree(6)])
+    >>> t3 = Tree(3, [t1, Tree(0), t2])
+    >>> print(t3)
+    3
+      1
+        3
+          4
+      0
+      2
+        5
+        6
+    >>> add_d_leaves(t3, 10)
+    >>> print(t3)
+    3
+      1
+        3
+          4
+            10
+            10
+            10
+          10
+          10
+        10
+      0
+        10
+      2
+        5
+          10
+          10
+        6
+          10
+          10
+        10
+    """
+    "*** YOUR CODE HERE ***"
+    def helper(tt, depth = 0):
+      for branch in tt.branches:
+        helper(branch, depth+1)
+      tt.branches.extend([Tree(v) for _ in range(depth)])
+    helper(t)
+
+def every_other(s):
+    """Mutates a linked list so that all the odd-indiced elements are removed
+    (using 0-based indexing).
+
+    >>> s = Link(1, Link(2, Link(3, Link(4))))
+    >>> every_other(s)
+    >>> s
+    Link(1, Link(3))
+    >>> odd_length = Link(5, Link(3, Link(1)))
+    >>> every_other(odd_length)
+    >>> odd_length
+    Link(5, Link(1))
+    >>> singleton = Link(4)
+    >>> every_other(singleton)
+    >>> singleton
+    Link(4)
+    """
+    "*** YOUR CODE HERE ***"
+    def helper(ss, index = 0):
+      if ss is Link.empty:
+        return
+      helper(ss.rest, index+1)
+      if index%2==0 and ss.rest != Link.empty:
+        # print("DEBUG: ",ss, ss.rest)
+        ss.rest = ss.rest.rest
+        # print("DEBUG: ",ss)
+    helper(s)
+
+
+def prune_small(t, n):
+    """Prune the tree mutatively, keeping only the n branches
+    of each node with the smallest label.
+
+    >>> t1 = Tree(6)
+    >>> prune_small(t1, 2)
+    >>> t1
+    Tree(6)
+    >>> t2 = Tree(6, [Tree(3), Tree(4)])
+    >>> prune_small(t2, 1)
+    >>> t2
+    Tree(6, [Tree(3)])
+    >>> t3 = Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2), Tree(3)]), Tree(5, [Tree(3), Tree(4)])])
+    >>> prune_small(t3, 2)
+    >>> t3
+    Tree(6, [Tree(1), Tree(3, [Tree(1), Tree(2)])])
+    """
+    while len(t.branches)>n:
+        largest = max(t.branches, key=lambda x:x.label)
+        t.branches.remove(largest)
+    for branch in t.branches:
+        prune_small(branch, n)
+
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
